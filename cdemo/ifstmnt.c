3d39e6ec99b6fcbabda4d51c03d8e81c827ea103
Haley Elliott <haleyhelliott@gmail.com>
2019-05-23 20:46:56 +0000

    ifstmnt

---
+++ cdemo/ifstmnt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+
+	if (a==0)
+	{
+		printf("a is equal to 0\n");
+	}
+	else
+	{
+		printf("a is not equal to 0\n");
+	}
+
+	int b = 2;
+        if (a==b)
+        {
+                printf("a is equal to b\n");
+        }
+        else if (a != b)
+        {
+                printf("a is not equal to b\n");
+        }
+	else if (a > b)
+	{
+		printf("a is greater than b\n");
+	}
+	else if (a >= b)
+	{
+		printf("a is greater than or equal to b\n");
+	}
+	else if (a == 0 && b == 0)
+	{
+		printf("a is equal to 0 AND b is equal to 0\n");
+	}
+	else if (a == 0 || b == 0)
+	{
+		printf("a is equal to 0 OR b is equal to 0\n");
+	}
+	else if (!a == 0)
+	{
+		printf("a is NOT equal to 0\n");
+	}
+}
