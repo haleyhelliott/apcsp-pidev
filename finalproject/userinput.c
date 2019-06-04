e0f25718f0ac426e77fbcc8cf13136b3ed27bc81
Haley Elliott <haleyhelliott@gmail.com>
2019-06-04 18:09:11 +0000

    userinput1

--- finalproject/userinput.c
+++ finalproject/userinput.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
+
+int main(int* array)
+{
+	int n;
+	n = sizeof(array)/sizeof(array[0]);
+	if (n != 5)
+	{
+		printf("Not the correct number of values. Try again.\n");
+	}
+	else if (n = 5)
+	{
+		printf("This is your code: %d\n", array[0]);
+	}
+}
